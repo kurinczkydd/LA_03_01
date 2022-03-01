f24f9c3510fff9f1547bec01f9de23f350a4ecae
BudaiDaniel <budai.daniel03@gmail.com>
2022-03-01 12:50:27 +0100

    HeroEditor Created

---
+++ LA_03_01/Services/EditorViaWindow.cs
@@ -0,0 +1,17 @@
+﻿using LA_03_01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA_03_01.Services
+{
+    public class EditorViaWindow : ISuperHeroEditorService
+    {
+        public void Edit(SuperHero superHero)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
