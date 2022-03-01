74e5bb58da6b381d4acfc17e9064c93fa00a8029
BudaiDaniel <budai.daniel03@gmail.com>
2022-03-01 12:28:31 +0100

    Logic Finished + Interface

---
+++ LA_03_01/Logic/ISuperHeroLogic.cs
@@ -0,0 +1,15 @@
+﻿using LA_03_01.Models;
+using System.Collections.Generic;
+
+namespace LA_03_01.Logic
+{
+    interface ISuperHeroLogic
+    {
+        double AVGPower { get; }
+        double AVGSpeed { get; }
+
+        void AddtoArmy(SuperHero superHero);
+        void RemovefromArmy(SuperHero superHero);
+        void SetupCollection(IList<SuperHero> barrack, IList<SuperHero> army);
+    }
+}
