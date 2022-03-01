5ffe621a9ed62a86332ae3f6bbd0b65afc3011da
BudaiDaniel <budai.daniel03@gmail.com>
2022-03-01 12:47:05 +0100

    Small changes

---
+++ LA_03_01/Services/ISuperHeroEditorService.cs
@@ -0,0 +1,14 @@
+﻿using LA_03_01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LA_03_01.Services
+{
+    public interface ISuperHeroEditorService
+    {
+        void Edit(SuperHero superHero);
+    }
+}
